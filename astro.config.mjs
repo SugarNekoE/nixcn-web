6845bcb2088ffc06dbfdb5b3ddf3d39c13251a21
Asai Neko <sugar@sne.moe>
2025-12-08 00:06:01 +0800

    Impl prettier to project
    - Add prettier json config
    - Format project with prettier config
    - Enable prettier in zed project settings
    
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,35 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
-import starlightThemeGalaxy from "starlight-theme-galaxy";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeGalaxy from 'starlight-theme-galaxy';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "NixCN",
-      defaultLocale: "root",
-      locales: {
-        root: {
-          label: "简体中文",
-          lang: "zh-CN",
-        },
-        en: {
-          label: "English",
-        },
-      },
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/withastro/starlight",
-        },
-      ],
-      sidebar: [],
-      plugins: [starlightThemeGalaxy()],
-    }),
-    mdx(),
-  ],
+    integrations: [
+        starlight({
+            title: 'NixCN',
+            defaultLocale: 'root',
+            locales: {
+                root: {
+                    label: '简体中文',
+                    lang: 'zh-CN',
+                },
+                en: {
+                    label: 'English',
+                },
+            },
+            social: [
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/withastro/starlight',
+                },
+            ],
+            sidebar: [],
+            plugins: [starlightThemeGalaxy()],
+        }),
+        mdx(),
+    ],
 });
